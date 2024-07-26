3aa04c611e834f4ea82ed6036b3074756244cc3d
Robert Stelmach <robert.stelmach7@wp.pl>
2024-07-26 19:57:51 +0200

    remove unnecessary logs

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,7 @@ async fn main() {
         .init();
 
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    log::info!("Database URL: {:?}", db_url);
-
     let config = AsyncDieselConnectionManager::<AsyncPgConnection>::new(db_url.clone());
-    log::info!("Config: {:?}", config);
 
     let pool = match bb8::Pool::builder().build(config).await {
         Ok(pool) => pool,
@@ -34,7 +31,6 @@ async fn main() {
             return;
         }
     };
-    log::info!("Pool created: {:?}", pool);
 
     let app = routes::create_routes(pool);
 
